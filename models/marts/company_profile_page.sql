6fde0be0fface2b99db3e375dd7f758639988492
lptais <laurindo.tais@gmail.com>
2025-06-21 22:05:07 +0200

    fixes company marts and contract

--- models/marts/company_profile_page.sql
+++ models/marts/company_profile_page.sql
@@ -7,9 +7,9 @@ select
     procurement_number,
     source,
     title as procurement_title,
-    publish_date,
+    publish_date as published_date,
     buyer,
-    value_amount,
+    value_amount as bid_value,
     currency,
     is_winner
 from {{ ref('int_suppliers_bids') }}
